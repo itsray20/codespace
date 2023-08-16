fb10d086bfbc162fc8d440a5097beaa39a4411ae
Rohan Bedage <141174898+rohansbedage@users.noreply.github.com>
2023-08-16 23:31:52 +0530

    Adding PHP Display Name and Address

---
+++ Php/Basic Programs/PHP Display Name and Address.php
@@ -0,0 +1,26 @@
+
+<html>
+  
+<center>
+    <br><br>
+<h1>
+<?php echo 'Display Name & Address ' ?>
+  </h1> <br><br>
+    <div class=" text-center">
+        <h1>
+            <?php echo 'My Name is : Rohan ' ?>
+        </h1>
+        <h1 class="text-bold">
+            <?php echo '&' ?>
+        </h1>
+       
+        <h1>
+            <?php echo 'My Address is :' ?>    <?php echo 'Viman Nagar Pune' ?>
+        </h1>
+      
+    
+
+    </div>
+    </center>
+    </html>
+
