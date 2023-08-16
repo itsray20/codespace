2ae5642237fcb54ce135b9c609fce8b715345a8b
Rohan Bedage <141174898+rohansbedage@users.noreply.github.com>
2023-08-16 23:30:39 +0530

    Adding Php Work

---
+++ Php/Basic Programs/PHP Echo and Print.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<center>
+<h2>Echo & Print In Php</h2>
+<body>
+
+<?php
+echo"<br><br>";
+echo "WELCOME TO LEARN PHP" ;
+echo"<br>";
+echo ("WELCOME TO LEARN PHP");
+echo"<br>";
+echo 'WELCOME TO LEARN PHP';
+echo"<br>";
+echo "WELCOME TO LEARN PHP";
+echo"<br>";
+print "************************************";
+print"<br>";
+print "WELCOME TO LEARN PHP" ;
+print"<br>";
+print("WELCOME TO LEARN PHP");
+print"<br>";
+print'WELCOME TO LEARN PHP';
+print"<br>"; 
+print"WELCOME TO LEARN PHP";
+print"<br>";
+?>
+
+</body>
+</center>
+</html>
